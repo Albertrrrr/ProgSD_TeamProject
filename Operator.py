805305e0a7a1898963e0dabd48159b7d24fa1c1b
RuixianZhang <zhangruixian98@vip.qq.com>
2023-10-19 08:08:55 +0100

    创建Operator

---
+++ Operator.py
@@ -0,0 +1,23 @@
+class Operator:
+    def __init__(self,mail, par=None):
+        self.__id = 0
+        self.__name = par[0]
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+
+
