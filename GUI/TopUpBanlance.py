bb9da95bc9f2f2afecf2775267488b701e3648d2
282604691 <93993202+282604691@users.noreply.github.com>
2023-10-22 14:52:27 +0100

    前端美化1

--- GUI/TopUpBanlance.py
+++ GUI/TopUpBanlance.py
@@ -2,38 +2,44 @@ from tkinter import *
 from tkinter import font
 
 window = Tk()
-window.geometry("1300x800")
-window.title("Bike-sharing system")
-l1_font = font.Font(family = "Times New Roman", size = 24)
-label1 = Label(text = "Top-up Banlance", font = l1_font)
-label1.place(x = 500, y = 100)
+window.geometry("1000x600")
+window.title("Bike-sharing System")
 
-l2_font = font.Font(family = "Times New Roman", size = 14)
-label2 = Label(text = "Current balance:", font = l2_font)
-label2.place(x = 330, y = 200)
-label3 = Label(text = "Card holder name:", font = l2_font)
-label3.place(x = 315, y = 260)
-label4 = Label(text = "Card number:", font = l2_font)
-label4.place(x = 350, y = 320)
-label5 = Label(text = "Expiration date:", font = l2_font)
-label5.place(x = 336, y = 380)
-label6 = Label(text = "Top-up amount:", font = l2_font)
-label6.place(x = 330, y = 460)
-textbox1 = Entry(text = "")
-textbox1.place(x = 480, y = 260, width = 350, height = 25)
-textbox2 = Entry(text = "")
-textbox2.place(x = 480, y = 320, width = 350, height = 25)
-textbox3 = Entry(text = "")
-textbox3.place(x = 480, y = 380, width = 200, height = 25)
-textbox4 = Entry(text = "")
-textbox4.place(x = 480, y = 460, width = 350, height = 25)
+# Define fonts
+l1_font = font.Font(family="Helvetica", size=36, weight='bold')
+l2_font = font.Font(family="Helvetica", size=16)
+b_font = font.Font(family="Helvetica", size=14)
 
+# Label 1
+label1 = Label(text="Top-up Balance", font=l1_font)
+label1.place(x=350, y=50)
 
-b_font = font.Font(family = "Times New Roman", size = 13)
-button1 = Button(text = "GoBack", font = b_font)
-button1.place(x = 400, y = 650, width = 100, height = 40)
-button2 = Button(text = "Top-up", font = b_font)
-button2.place(x = 700, y = 650, width = 100, height = 40)
+# Labels for Current Balance, Card Holder Name, Card Number, Expiration Date, and Top-up Amount
+label2 = Label(text="Current Balance:", font=l2_font)
+label2.place(x=320, y=200)
+label3 = Label(text="Card Holder:", font=l2_font)
+label3.place(x=358, y=260)
+label4 = Label(text="Card Number:", font=l2_font)
+label4.place(x=348, y=320)
+label5 = Label(text="Expiration Date:", font=l2_font)
+label5.place(x=325, y=380)
+label6 = Label(text="Top-up Amount:", font=l2_font)
+label6.place(x=328, y=460)
 
+# Entry Widgets
+textbox1 = Entry(font=l2_font)
+textbox1.place(x=480, y=260, width=350, height=30)
+textbox2 = Entry(font=l2_font)
+textbox2.place(x=480, y=320, width=350, height=30)
+textbox3 = Entry(font=l2_font)
+textbox3.place(x=480, y=380, width=200, height=30)
+textbox4 = Entry(font=l2_font)
+textbox4.place(x=480, y=460, width=350, height=30)
+
+# Buttons
+button1 = Button(text="Go Back", font=b_font, bg='red', fg='white')
+button1.place(x=300, y=525, width=100, height=40)
+button2 = Button(text="Top-up", font=b_font, bg='green', fg='white')
+button2.place(x=600, y=525, width=100, height=40)
 
 window.mainloop()
--- GUI/TrackVehicle.py
+++ GUI/TrackVehicle.py
@@ -3,39 +3,41 @@ from tkinter import font
 from tkinter import ttk
 
 window = Tk()
-window.geometry("1300x800")
-window.title("Bike-sharing system")
-l1_font = font.Font(family = "Times New Roman", size = 24)
-label1 = Label(text = "Track Vehicle", font = l1_font)
-label1.place(x = 500, y = 100)
+window.geometry("1000x600")
+window.title("Bike-sharing System")
 
-l2_font = font.Font(family = "Times New Roman", size = 14)
-label2 = Label(text = "Vehicle ID", font = l2_font)
-label2.place(x = 200, y = 220)
-label3 = Label(text = "City Location", font = l2_font)
-label3.place(x = 180, y = 280)
+# Define fonts
+l1_font = font.Font(family="Helvetica", size=36, weight='bold')
+l2_font = font.Font(family="Helvetica", size=16)
+b_font = font.Font(family="Helvetica", size=14)
 
-def on_select(event):
-    selected_value = combo_var.get()
-    print("Selected value:", selected_value)
+# Label 1
+label1 = Label(text="Track Vehicle", font=l1_font)
+label1.place(x=350, y=50)
 
+# Labels for Vehicle ID and City Location
+label2 = Label(text="Vehicle ID", font=l2_font)
+label2.place(x=150, y=200)
+label3 = Label(text="City Location", font=l2_font)
+label3.place(x=150, y=250)
+
+# Comboboxes
 options = ["Option 1", "Option 2", "Option 3", "Option 4"]
-combo_var = StringVar(value=options[0])
-combobox = ttk.Combobox(textvariable=combo_var, values=options)
-combobox.bind("<<ComboboxSelected>>", on_select)
-combobox.place(x = 350, y = 220)
+combo_var1 = StringVar(value=options[0])
+combobox1 = ttk.Combobox(textvariable=combo_var1, values=options)
+combobox1.place(x=300, y=200)
+combo_var2 = StringVar(value=options[0])
+combobox2 = ttk.Combobox(textvariable=combo_var2, values=options)
+combobox2.place(x=300, y=250)
 
-combobox2 = ttk.Combobox(textvariable=combo_var, values=options)
-combobox2.bind("<<ComboboxSelected>>", on_select)
-combobox2.place(x = 350, y = 280)
+# Track and Back Buttons
+button1 = Button(text="Track", font=b_font, bg='green', fg='white')
+button1.place(x=700, y=190, width=100, height=40)
+button2 = Button(text="Back", font=b_font, bg='red', fg='white')
+button2.place(x=700, y=250, width=100, height=40)
 
-b_font = font.Font(family = "Times New Roman", size = 13)
-button1 = Button(text = "Track", font = b_font)
-button1.place(x = 800, y = 200, width = 100, height = 40)
-button2 = Button(text = "Back", font = b_font)
-button2.place(x = 800, y = 280, width = 100, height = 40)
-
-text_box = Text(window, height=10, width=40)
-text_box.place(x=300, y=400, height=300, width=500)
+# Text Box
+text_box = Text(window, wrap=WORD, height=10, width=40, font=l2_font)
+text_box.place(x=300, y=350, height=200, width=400)
 
 window.mainloop()
