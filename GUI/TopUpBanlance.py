dab50cfc5270c92c45129482fcd141399fbde078
282604691 <93993202+282604691@users.noreply.github.com>
2023-10-22 20:55:50 +0100

    前端美化3

--- GUI/TopUpBanlance.py
+++ GUI/TopUpBanlance.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import font
 
+from PIL import ImageTk
+
 window = Tk()
 window.geometry("1000x600")
 window.title("Bike-sharing System")
@@ -15,31 +17,37 @@ label1 = Label(text="Top-up Balance", font=l1_font)
 label1.place(x=350, y=50)
 
 # Labels for Current Balance, Card Holder Name, Card Number, Expiration Date, and Top-up Amount
-label2 = Label(text="Current Balance:", font=l2_font)
-label2.place(x=320, y=200)
-label3 = Label(text="Card Holder:", font=l2_font)
-label3.place(x=358, y=260)
-label4 = Label(text="Card Number:", font=l2_font)
-label4.place(x=348, y=320)
-label5 = Label(text="Expiration Date:", font=l2_font)
-label5.place(x=325, y=380)
-label6 = Label(text="Top-up Amount:", font=l2_font)
-label6.place(x=328, y=460)
+label2 = Label(text="Recharge amount:", font=l2_font)
+label2.place(x=180, y=200)
+label3 = Label(text="QR code:", font=l2_font)
+label3.place(x=600, y=200)
+
+label6 = Label(text="Notification:", font=l2_font)
+label6.place(x=180, y=250)
 
 # Entry Widgets
-textbox1 = Entry(font=l2_font)
-textbox1.place(x=480, y=260, width=350, height=30)
-textbox2 = Entry(font=l2_font)
-textbox2.place(x=480, y=320, width=350, height=30)
+# textbox1 = Entry(font=l2_font)
+# textbox1.place(x=480, y=260, width=350, height=30)
+# textbox2 = Entry(font=l2_font)
+# textbox2.place(x=480, y=320, width=350, height=30)
 textbox3 = Entry(font=l2_font)
-textbox3.place(x=480, y=380, width=200, height=30)
+textbox3.place(x=360, y=200, width=68, height=30)
 textbox4 = Entry(font=l2_font)
-textbox4.place(x=480, y=460, width=350, height=30)
+textbox4.place(x=180, y=280, width=250, height=200)
 
 # Buttons
 button1 = Button(text="Go Back", font=b_font, bg='red', fg='white')
 button1.place(x=300, y=525, width=100, height=40)
 button2 = Button(text="Top-up", font=b_font, bg='green', fg='white')
 button2.place(x=600, y=525, width=100, height=40)
+
+
+# Load and display an image
+image_path = "G:\ProgSD_TeamProject\qrcode_image\qr_test_ali_14.png"  # Change this to your image file path
+img = PhotoImage(file=image_path)
+img = img.subsample(2)  # Adjust the subsample factor to resize the image (e.g., 2 halves the size)
+
+image_label = Label(image=img)
+image_label.place(x=620, y=270)
 
 window.mainloop()
